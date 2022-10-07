80779501e7c99716a6fe230b94ae644811555095
Vitor Gabriel Linas <103148031+VitorLinas@users.noreply.github.com>
2022-10-07 15:10:55 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,9 @@
+function cadastrar(){
+    window.alert("Formulário ilustrativo!");
+}
+
+window.sr = ScrollReveal({reset: true});
+sr.reveal('.casaProjetos', {
+    duration: 1000,
+    rotate:{x:0, y:100, z: 0}
+});
